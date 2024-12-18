4536b587bb2f6a7d3339153310410cb55e771c4d
ozcannn22 <ceviko946@gmail.com>
2024-12-18 13:51:16 +0300

    update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: './index.html',
+      },
+    },
+  },
+  resolve: {
+    alias: {
+      './data': './src/data',
+    },
+  },
 })
